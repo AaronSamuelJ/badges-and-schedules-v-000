ecb2e3654e6ef49c46b6c78bd48b119ec6524f72
Aaron Samuel <aasamuelj@gmail.com>
2017-09-24 12:56:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,10 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |attendee|
+    badges << badge_maker(attendee)
+  end
+  badges
+end
